d81170ad7014ea5366a749cd9d2fe441cfd64297
kamerais <kubickimarcin102@gmail.com>
2024-01-03 17:02:08 +0100

    Tworzę logikę dla programu i ustawiam liczbę pod przyciskiem

--- 08. random number generator/main.js
+++ 08. random number generator/main.js
@@ -21,3 +21,14 @@ randomNum = Math.floor(Math.random()*(max-min)) + min;
 //                                              (max - min)         +                           min
 console.log(randomNum)
 */
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max = 6;
+let randomNum;
+
+myButton.onclick = function(){
+    randomNum = Math.floor(Math.random() * max) + min;
+    myLabel.textContent = randomNum;
+}
