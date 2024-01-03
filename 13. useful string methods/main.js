f3e382eff85b951663d2009d945e50f36c478cfe
kamerais <kubickimarcin102@gmail.com>
2024-01-04 00:12:39 +0100

    Przykład metody charAt(nr.indexu)

--- 13. useful string methods/main.js
+++ 13. useful string methods/main.js
@@ -1,1 +1,6 @@
 // string methods = pozwalają na manipulowanie i pracę z tekstem (stringiem)
+
+let userName = "kamerais";
+
+//metoda .charAt(nr.indexu) pokazuje jaka literka znajduję się w podanym indexie
+console.log(userName.charAt(0)); // output: k
