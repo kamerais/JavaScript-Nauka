0dbf2bcf7170c5cc7e8e488ad7a715a00916eced
kamerais <kubickimarcin102@gmail.com>
2024-01-03 14:12:19 +0100

    Przykład dlaczego korzystamy z const

--- 05. const/main.js
+++ 05. const/main.js
@@ -11,3 +11,8 @@ circumference = 2 * pi * radius;
 
 console.log(circumference); //output 12.56 (jeżeli input = 2)
 
+// Dlaczego używamy const? Bo ktoś może przypadkiem zmienić wartość zmiennej, która powinna być stała no, pi
+pi = 31.4
+circumference = 2 * pi * radius;
+
+console.log(circumference) //output 125.6 (jeżeli input = 2)
