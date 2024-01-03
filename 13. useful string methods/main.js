ec74ce212a89c08bd884126ca0989d269cef52ef
kamerais <kubickimarcin102@gmail.com>
2024-01-04 00:14:38 +0100

    Przykład metody .indexOf("Literka z tekstu")

--- 13. useful string methods/main.js
+++ 13. useful string methods/main.js
@@ -4,3 +4,7 @@ let userName = "kamerais";
 
 //metoda .charAt(nr.indexu) pokazuje jaka literka znajduję się w podanym indexie
 console.log(userName.charAt(0)); // output: k
+
+// metoda indexOf("literka z tekstu") mówi na którym indexie od 0 do n - 1 znajduje się literka
+
+console.log(userName.indexOf("r"));
