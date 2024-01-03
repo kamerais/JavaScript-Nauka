786713f0341daa1d28eca039be8d6e761cf72962
kamerais <kubickimarcin102@gmail.com>
2024-01-03 23:43:17 +0100

    Przykład zastosowania ternary operatora

--- 11. ternary operator/main.js
+++ 11. ternary operator/main.js
@@ -1,3 +1,16 @@
 // ternary operator = to skrót do if{} i else{} statements
 //                    pomaga ustalić co ma mieć w sobie zmienna za pomocą warunku
 //                    syntax: condition ? codeIfTrue : codeIfFalse
+
+let age = 21;
+let message = age >= 18 ? "You are an adult" : "You are a minor";
+
+//alternatywa
+/*
+if (age >= 18){
+    message = "You are an adult";
+}
+else{
+    message = "Your are a minor"
+}
+*/
