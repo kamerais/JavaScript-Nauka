7e63de309813177ee9e5cf45252dcfcddaea6f5a
kamerais <kubickimarcin102@gmail.com>
2024-01-03 12:08:18 +0100

    Można przypisać wartość od razu przy deklaracji zmiennej

--- 01. variables/main.js
+++ 01. variables/main.js
@@ -4,7 +4,13 @@
 // 1. deklaracja        let x;
 // 2. przypisanie       x = 100;
 
-let x;
-x = 100;
+//let x;
+//x = 100;
 
-console.log(x); // wyświetla w konsoli w narzędziach developera na stronie liczbę 100.
+//console.log(x); // wyświetla w konsoli w narzędziach developera na stronie liczbę 100.
+
+// można dokonać deklaracji i przypisania jednocześnie
+
+let x = 100;
+
+console.log(x); // To też wyświetli 100 (Trzeba pamiętać, że nie można zadeklarować 2 razy tej samej zmiennej)
