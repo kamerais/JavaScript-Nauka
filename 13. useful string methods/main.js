7d8a6ab03c3512c11b162c1e8a89bcec0fb16bda
kamerais <kubickimarcin102@gmail.com>
2024-01-04 00:21:43 +0100

    Przykład zastosowania metody .lastIndexOf oraz przypadek, gdy w tekście jest kilka takich samych literek

--- 13. useful string methods/main.js
+++ 13. useful string methods/main.js
@@ -7,4 +7,17 @@ console.log(userName.charAt(0)); // output: k
 
 // metoda indexOf("literka z tekstu") mówi na którym indexie od 0 do n - 1 znajduje się literka
 
-console.log(userName.indexOf("r"));
+console.log(userName.indexOf("r")); //output: 4
+
+// metoda .lastIndexOf("Literka z tekstu") mówi na którym indexie od n - 1 do 0 znajduje się literka
+
+console.log(userName.lastIndexOf("r")); //output: 4
+
+// Jeżeli mój userName miałby więcej takich samych liter to pierwszy znaleziony i ostatni znaleziony index
+// miałyby inne wartości
+//Przykład
+
+userName = "Hakunamatata";
+
+console.log(userName.indexOf("a")); //output: 1
+console.log(userName.lastIndexOf("a")); //output: 11
