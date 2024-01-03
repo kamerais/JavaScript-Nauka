b95883b9ed5e12b1ab684de01b115a4c8e22d75f
kamerais <kubickimarcin102@gmail.com>
2024-01-03 17:19:53 +0100

    Drugi przykład, ale warunek jest fałszywy

--- 09. if statements/main.js
+++ 09. if statements/main.js
@@ -1,5 +1,6 @@
 // if statements (instrukcja warunkowa) = Jeżeli warunek jest spełniony to wykonaj kod
 //                                        jeżeli nie to zrób coś innego
+/*
 
 let age = 25;
 
@@ -8,4 +9,14 @@ if(age >= 18) { // Jeżeli zmienna age większa lub równa 18 to wykonaj ten kod
 }
 else{ // w przeciwnym razie wykonaj ten kod
     console.log("You must be 18+ to enter this site")
+}
+
+*/
+let time = 13; // powiedzmy, że to czas w godzinach
+
+if (time < 12){ // jeżeli czas jest mniejszy niż godzina 12 to wykonaj ten kod
+    console.log("Good morning")
+}
+else{ // w przeciwnym razie wykonaj ten kod
+    console.log("Good afternoon")
 }
