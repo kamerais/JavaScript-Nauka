6e78b5a498bc6ce2c82a6334e86c4234de938b90
kamerais <kubickimarcin102@gmail.com>
2024-01-03 17:41:34 +0100

    Opis else if statements

--- 09. if statements/main.js
+++ 09. if statements/main.js
@@ -37,6 +37,9 @@ else{ // w przeciwnym razie wykonaj ten kod
 }
 
 */
+
+/*
+
 // Można tworzyć tzw. nested if-statements (zagnieżdżone instrukcje warunkowe)
 let age = 25;
 let hasLicense = false;
@@ -54,4 +57,30 @@ if(age >= 16){
 }
 else{
     console.log("You are not old enough to have a license");
+}
+
+*/
+// jak działają else if statements
+let age = 101;
+
+if(age >= 100){ // jeżeli wiek jest większy bądź równy 100 to wykonaj ten kod
+    console.log("You are too old to enter this site");
+}
+/*
+dlaczego zamieniłem age >= 18 z age >= 100 miejscami?
+Warunki są czytane od góry do dołu. Jakbym dał age >= 18 pierwsze to warunek zostałby spełniony
+co za tym idzie warunek age >= 100 nigdy by się nie wykonał, bo age = 101 jest większe od 18
+*/
+
+else if(age == 0){
+    console.log("You can't enter. You were just born.")
+}
+else if(age >= 18){ // jeżeli wiek jest większy bądź równy 18 to wykonaj ten kod
+    console.log("You are old enough to enter this site");
+}
+else if(age < 0){ // jeżeli wiek jest mniejszy od 0 to wykonaj ten kod
+    console.log("Your age can't be below 0");
+}
+else{ // w przeciwnym wypadku wykonaj ten kod
+    console.log("You must be 18+ to enter this site");
 }
