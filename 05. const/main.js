0411e639d4a05b58cf98887313604674f039c294
kamerais <kubickimarcin102@gmail.com>
2024-01-03 14:10:45 +0100

    Przykład bez const

--- 05. const/main.js
+++ 05. const/main.js
@@ -1,1 +1,13 @@
 // const (stała) = zmienna, której nie można zmienić
+
+let pi = 3.14;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
+
+circumference = 2 * pi * radius;
+
+console.log(circumference); //output 12.56 (jeżeli input = 2)
+
