f4c81e8b20c25f0f3b085a0ec8bdd5c9eee915dd
kamerais <kubickimarcin102@gmail.com>
2024-01-03 17:16:41 +0100

    Przykład instrukcji warunkowej

--- 09. if statements/main.js
+++ 09. if statements/main.js
@@ -1,2 +1,11 @@
 // if statements (instrukcja warunkowa) = Jeżeli warunek jest spełniony to wykonaj kod
 //                                        jeżeli nie to zrób coś innego
+
+let age = 25;
+
+if(age >= 18) { // Jeżeli zmienna age większa lub równa 18 to wykonaj ten kod
+    console.log("You are old enough to enter this site")
+}
+else{ // w przeciwnym razie wykonaj ten kod
+    console.log("You must be 18+ to enter this site")
+}
