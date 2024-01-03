300f11e257305b4a39696da79e9b8959e1d02a1a
kamerais <kubickimarcin102@gmail.com>
2024-01-03 14:16:41 +0100

    Przykład const w praktyce

--- 05. const/main.js
+++ 05. const/main.js
@@ -1,14 +1,14 @@
 // const (stała) = zmienna, której nie można zmienić
 
-let pi = 3.14;
+//let pi = 3.14;
 let radius;
 let circumference;
 
 radius = window.prompt("Enter the radius of a circle");
 radius = Number(radius);
 
-circumference = 2 * pi * radius;
 
+/*
 console.log(circumference); //output 12.56 (jeżeli input = 2)
 
 // Dlaczego używamy const? Bo ktoś może przypadkiem zmienić wartość zmiennej, która powinna być stała no, pi
@@ -16,3 +16,12 @@ pi = 31.4
 circumference = 2 * pi * radius;
 
 console.log(circumference) //output 125.6 (jeżeli input = 2)
+*/
+
+//const w praktyce
+const pi = 3.14;
+circumference = 2 * pi * radius;
+console.log(circumference); //output 12.56 (jeżeli input = 2)
+
+pi = 31.4;
+console.log(circumference); // wyskakuje błąd "main.js:26 Uncaught TypeError: Assignment to constant variable."
