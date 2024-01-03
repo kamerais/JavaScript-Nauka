a45cc3c5056d8e7952f1e7fa65b23949b099994f
kamerais <kubickimarcin102@gmail.com>
2024-01-03 12:00:19 +0100

    Definicja zmiennej, jak się ją deklaruje i przypisuje

---
+++ 01. variables/main.js
@@ -0,0 +1,5 @@
+// varialble (czyli zmienna) = To taki kontener, który przechowuje jakąś wartość.
+//                              Zachowuje się jakoby by była tą wartością, którą przechowuje
+
+// 1. deklaracja        let x;
+// 2. przypisanie       x = 100;
