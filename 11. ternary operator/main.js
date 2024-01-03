f0db3bb1bcc75bf36c9835e95221a02abbf10bdf
kamerais <kubickimarcin102@gmail.com>
2024-01-03 23:38:30 +0100

    Definicja ternary operatora

---
+++ 11. ternary operator/main.js
@@ -0,0 +1,3 @@
+// ternary operator = to skrót do if{} i else{} statements
+//                    pomaga ustalić co ma mieć w sobie zmienna za pomocą warunku
+//                    syntax: condition ? codeIfTrue : codeIfFalse
