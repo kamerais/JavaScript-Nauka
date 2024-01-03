dbebe610e9f3137b093eb339d884558ae4edf8d8
kamerais <kubickimarcin102@gmail.com>
2024-01-03 12:49:18 +0100

    Tłumaczę kolejność wykonywania działań.

--- 02. arithmetic expressions/main.js
+++ 02. arithmetic expressions/main.js
@@ -31,3 +31,20 @@ console.log(students); // output: 1
 
 students --; // odejmuję 1
 console.log(students); // output: 0
+
+/*
+    operator precedence (kolejność wykonywania działań od lewej do prawej)
+    1. nawiasy ()
+    2. potęgowanie
+    3. mnożenie i dzielenie i modulo (reszta z dzielenia)
+    4. dodawania i odejmowanie
+*/
+let result = 1 + 2 * 3 + 4 ** 2; // najpierw potęguję, mnożę, a na końcu dodaję
+console.log(result); // output: 23
+
+result = 12 % 5 + 8 / 2; // tutaj szukam reszty z dzielenia, potem dzielę i dodaje lub na odwrót i dodaję
+// Dodawanie jest przemienne, czyli 12 % 5 + 8 / 2 = 8 / 2 + 12 % 5
+console.log(result); // output: 6
+
+result = 6 / 2 ** (2 + 5); //najpierw robię działanie w nawiasie, potem potęguję, a na końcu dzielę
+console.log(result) // output: 0.046875
