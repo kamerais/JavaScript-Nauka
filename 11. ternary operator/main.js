e6561dcb7c6be10f9f4c02637c2f1820b2cd20de
kamerais <kubickimarcin102@gmail.com>
2024-01-03 23:49:01 +0100

    Finalny przykład z zastosowaniem ternary operatora

--- 11. ternary operator/main.js
+++ 11. ternary operator/main.js
@@ -21,7 +21,13 @@ let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
 
 console.log(greeting);
 */
+/*
 let isStudent = true;
 let message = isStudent ? "You are a student" : "You are not a student";
 
 console.log(message);
+*/
+let purchaseAmount = 125;
+let discount = purchaseAmount >= 100 ? 10 : 0;
+
+console.log(`Your total is $${purchaseAmount - purchaseAmount * (discount/100)}`)
