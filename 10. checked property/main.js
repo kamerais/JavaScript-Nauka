df9e772ff2b798662c6f03c462d3f5242f40e9fa
kamerais <kubickimarcin102@gmail.com>
2024-01-03 18:46:32 +0100

    Dodaję logikę do programy sprawdzającego czy użytkownik zaznaczył subskrypcję i metodę płatności

--- 10. checked property/main.js
+++ 10. checked property/main.js
@@ -9,3 +9,26 @@ const paypalBtn = document.getElementById("paypalBtn");
 const mySubmit = document.getElementById("mySubmit");
 const subResult = document.getElementById("subResult");
 const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function(){
+
+    if(myCheckBox.checked){
+        subResult.textContent = "You are subscribed";
+    }
+    else{
+        subResult.textContent = "You are not subscribed";
+    }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = "You are paying with Visa";
+    }
+    else if(mastercardBtn.checked){
+        paymentResult.textContent = "You are paying with MasterCard";
+    }
+    else if(paypalBtn.checked){
+        paymentResult.textContent = "You are paying with Paypal";
+    }
+    else{
+        paymentResult.textContent = "You must select a payment type";
+    }
+}
