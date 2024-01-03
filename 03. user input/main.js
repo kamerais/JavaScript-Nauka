4eac4c3ec7d22e59450c4715adea7f4b21ce6d8f
kamerais <kubickimarcin102@gmail.com>
2024-01-03 13:34:06 +0100

    Dodaję id do <h1> i je modyfikuję razem z user input

--- 03. user input/main.js
+++ 03. user input/main.js
@@ -16,5 +16,6 @@ let username;
 
 document.getElementById("mySubmit").onclick = function(){
     username = document.getElementById("myText").value;
-    console.log(username);
+    //console.log(username);
+    document.getElementById("myH1").textContent = `Hello ${username}`
 }
