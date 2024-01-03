af2989822131c97f51b8ff0fbd76b0341743d743
kamerais <kubickimarcin102@gmail.com>
2024-01-03 13:23:19 +0100

    Pokazanie prostszego sposobu na user input

--- 03. user input/main.js
+++ 03. user input/main.js
@@ -2,3 +2,10 @@
     1. Prosty sposób = window prompt
     2. Profesjonalny sposób = HTML textbox
 */
+
+let username; // deklaruję zmienną
+
+username = window.prompt("Jak masz na imie?"); // Na stronie wyskoczy informacja z polem tekstowym i zapytaniem
+//                                                Jak masz na imie?
+
+console.log(username); // output: (jak wpisałem kamerais) kamerais
