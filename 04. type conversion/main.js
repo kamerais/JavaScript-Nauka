c26d183d5166cdc55380bdd874cba80080e58cf7
kamerais <kubickimarcin102@gmail.com>
2024-01-03 13:49:55 +0100

    Przykład co się dzieje, jeżeli do stringu dodamy liczbę

--- 04. type conversion/main.js
+++ 04. type conversion/main.js
@@ -1,3 +1,9 @@
 /* type conversion (kowersja typu zmiennej) = zmiana typu danych z jednego typu wartości na drugi
                                             (strings, numbers, booleans)
 */
+
+let age = window.prompt("How old are you?"); //typ string
+
+age += 1; // typ int
+
+console.log(age); // output: np. 251, jeżeli input = 25
