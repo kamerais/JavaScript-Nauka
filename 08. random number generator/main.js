924f60423e202c28caa1c5e017767ada65714bcb
kamerais <kubickimarcin102@gmail.com>
2024-01-03 16:58:56 +0100

    Ustawiam wszystko do stworzenia programy i wykomentowywuje teorię

---
+++ 08. random number generator/main.js
@@ -0,0 +1,23 @@
+// Random Number Generator
+/* Teoria
+
+let randomNum = Math.random(); // losuje liczbę między 0, a 1
+
+console.log(randomNum);
+
+// Jeżeli chcę losować liczbę całkowitę od 1 do 6
+
+randomNum = Math.floor(randomNum * 6) + 1; // zaokrąglam w dół (dostaję liczby od 0-5) i dodaję 1 (dostaję liczby 1-6)
+
+console.log(randomNum); //output: losowa liczba całkowita od 1 do 6
+
+// Jeżeli chcę wylosować liczbę z jakiegoś przedziału
+
+const min = 50;
+const max = 100;
+
+randomNum = Math.floor(Math.random()*(max-min)) + min;
+// zaokrąglam w dół liczbę z przedziału od 0-50 (100 - 50 = 50) i dodaję do zaokrąglonej liczby 50
+//                                              (max - min)         +                           min
+console.log(randomNum)
+*/
