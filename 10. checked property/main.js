a25fcaafd9ce118335b34471cbb2183103fc4149
kamerais <kubickimarcin102@gmail.com>
2024-01-03 18:41:16 +0100

    Definicja własności .checked i przypisanie zmiennym ich id z HTML.

---
+++ 10. checked property/main.js
@@ -0,0 +1,11 @@
+// .checked = własność, która określa jaki jest stan elementu HTML przycisku checkbox lub radio.
+//                                 (zaznaczony czy niezaznaczony)
+
+
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const mastercardBtn = document.getElementById("mastercardBtn");
+const paypalBtn = document.getElementById("paypalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
