ab41c4bf24a949d82fadfe77e8bb5992d1a74f91
kamerais <kubickimarcin102@gmail.com>
2024-01-03 13:32:38 +0100

    Pokazuję jak dostać user input w sposób profesjonalny.

--- 03. user input/main.js
+++ 03. user input/main.js
@@ -3,9 +3,18 @@
     2. Profesjonalny sposób = HTML textbox
 */
 
+/*
 let username; // deklaruję zmienną
 
 username = window.prompt("Jak masz na imie?"); // Na stronie wyskoczy informacja z polem tekstowym i zapytaniem
 //                                                Jak masz na imie?
 
 console.log(username); // output: (jak wpisałem kamerais) kamerais
+*/
+
+let username;
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    console.log(username);
+}
