9240dac50f7425f9b215d8f44609f57308a5632c
kamerais <kubickimarcin102@gmail.com>
2024-01-04 16:56:35 +0100

    Odpowiedź do ćwiczenia na dwa różne sposoby

--- 18. while loops/main.js
+++ 18. while loops/main.js
@@ -1,6 +1,6 @@
 // while loop (pętla) = powtarza kod tyle raz ile razy DOPÓKI spełnia warunki
 
-let username ="";
+//let username ="";
 
 //Przykład pętli, która nigdy się nie skończy
 /*
@@ -25,6 +25,7 @@ console.log(`Hello ${username}`); // output: Hello kamerais
 // Z tego powodu mozna byłoby nie przypisywać, żadnej wartości dla username tylko zostawić let username;
 // Wtedy użytkownik byłby poproszony o wprowadzenie username, jeżeli nie spełni to warunków to po prosi kolejny raz,
 // aż nie wprowadzi czegoś do username
+/*
 let nick;
 do{
     nick = window.prompt("Enter your username: "); // prosi o nick
@@ -32,3 +33,41 @@ do{
 
 // input: kamerais
 console.log(`Hello ${nick}`); // output: Hello kamerais
+*/
+//Ćwiczenie:
+// Sprawdź czy użytkownik jest zalogowany
+// Aby się zalogować musi podać swój username i password
+// jeżeli się zaloguje ma zostać przywitany
+// Jeżeli nick nie zostanie wprowadzony ma poprosić o nick
+// Jeżeli password nie zostanie wprowadzony ma poprosić o password
+
+let isLoggedIn = false;
+let username;
+let password;
+
+while(!isLoggedIn){
+    username = window.prompt("Enter your username: ");
+    password = window.prompt("Enter your password: ");
+    if(username === "myUsername" && password == "myPassword"){
+        isLoggedIn = true;
+        console.log(`Welcome ${username}`);
+    }
+    else{
+        console.log("Nieprawidłowe dane, proszę powtórzyć");
+
+    }
+}
+
+do{
+    username = window.prompt("Enter your username: ");
+    password = window.prompt("Enter your password: ");
+    if(username === "myUsername" && password == "myPassword"){
+        isLoggedIn = true;
+        console.log(`Welcome ${username}`);
+    }
+    else{
+        console.log("Nieprawidłowe dane, proszę powtórzyć");
+
+    }
+}while(!isLoggedIn)
+
