7d3044a6ac8635aa43db03dbff4ff7448e5d924b
kamerais <kubickimarcin102@gmail.com>
2024-01-04 01:05:54 +0100

    Poprawa definicji oraz przykład zastosowania

--- 14. string slicing/main.js
+++ 14. string slicing/main.js
@@ -1,4 +1,12 @@
 // string slicing = tworzenie podstringów
 //                  z porcji innego stringu
 
-//                  string.slice(start,end)
+//                  string.slice(start,end - 1)
+
+const fullName = "kamerais";
+
+let firstName = fullName.slice(0,4); //jeżeli dałbym .slice(0,3)
+console.log(firstName); //output: kame, a przy .slice(0,3) output: kam
+
+let lastName = fullName.slice(4,8); // lub poprostu .slice(4) samo dopisze koniec
+console.log(lastName); //output: rais
