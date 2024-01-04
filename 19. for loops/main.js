998a70bedf839acab07f7e465f903f1a3b7b299e
kamerais <kubickimarcin102@gmail.com>
2024-01-04 17:10:09 +0100

    Przykłady pętli for loop

--- 19. for loops/main.js
+++ 19. for loops/main.js
@@ -1,1 +1,12 @@
 // for loop = wykonuje jakiś kod limitowaną ilość razy, kiedy while loop może być nieskończone
+
+// Przykład for loop wypisującego 3 razy Hello
+
+for(let i = 0; i <= 2; i++){
+    console.log("Hello!");
+}
+// Przykład kodu wypisującego 10 cyfr od 1 do 10
+
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
