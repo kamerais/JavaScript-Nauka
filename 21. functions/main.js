522fa1dd734af51a6f28f95f22b447d1ef7c2bba
kamerais <kubickimarcin102@gmail.com>
2024-01-04 19:02:18 +0100

    Jak napisać funkcję, która ma pewne parametry

--- 21. functions/main.js
+++ 21. functions/main.js
@@ -1,11 +1,20 @@
 // function (funkcja) = Sekcja reużywalnego kodu
 //                      Zadeklarowany raz kod, może zostać użyty kiedy chcesz
 //                      Wywołaj funkcję, abu uruchomić ten kod
-
+/*
 function happyBirthday(){
     console.log("Happy birthday to you");
     console.log("Happy birthday to you");
     console.log("Happy birthday dear you");
     console.log("Happy birthday to you");
 }
-happyBirthday()
+*/
+// funkcja wymagająca pewnych argumentów (parametry funkcji)
+function happyBirthday(username, age){
+    console.log("Happy birthday to you");
+    console.log("Happy birthday to you");
+    console.log(`Happy birthday dear ${username}`);
+    console.log("Happy birthday to you");
+    console.log(`You are ${age} years old`);
+}
+happyBirthday("kamerais", 19); // argumenty funkcji
