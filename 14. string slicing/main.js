2adf1a4c08d9ff130c12323122ae6ca6c5623ebc
kamerais <kubickimarcin102@gmail.com>
2024-01-04 01:14:10 +0100

    Przykład jak znaleźć pierwszą i ostatnią literkę

--- 14. string slicing/main.js
+++ 14. string slicing/main.js
@@ -10,3 +10,8 @@ console.log(firstName); //output: kame, a przy .slice(0,3) output: kam
 
 let lastName = fullName.slice(4,8); // lub poprostu .slice(4) samo dopisze koniec
 console.log(lastName); //output: rais
+
+let firstChar = fullName.slice(0,1);
+console.log(firstChar);
+
+let lastChar = fullName.slice(-1); //używając wartości mniejszych od 0 idzie się od końca do początku
