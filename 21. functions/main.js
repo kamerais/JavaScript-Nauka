ef660646d8a458a111b6ffb429b9164d45e8a588
kamerais <kubickimarcin102@gmail.com>
2024-01-04 18:56:35 +0100

    Definicja funkcji

---
+++ 21. functions/main.js
@@ -0,0 +1,3 @@
+// function (funkcja) = Sekcja reużywalnego kodu
+//                      Zadeklarowany raz kod, może zostać użyty kiedy chcesz
+//                      Wywołaj funkcję, abu uruchomić ten kod
