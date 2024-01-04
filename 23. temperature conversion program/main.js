c5178ba1aa804f653f6d00b34d5af46c16fbf4ff
kamerais <kubickimarcin102@gmail.com>
2024-01-05 00:56:22 +0100

    Przygotowanie funkcji do konwersji jednostek

--- 23. temperature conversion program/main.js
+++ 23. temperature conversion program/main.js
@@ -1,5 +1,19 @@
 // Temperature conversion program
 
+const textBox = document.getElementById("textbox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+
 function convert(){
-    
+    if(toFahrenheit.checked){
+
+    }
+    else if(toCelsius.checked){
+
+    }
+    else{
+        result.textContent = "Select a unit";
+    }
 }
