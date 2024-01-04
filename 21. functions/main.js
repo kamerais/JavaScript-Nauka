3a3137c8f06eb4588cec284fb8671433b8108667
kamerais <kubickimarcin102@gmail.com>
2024-01-04 19:07:56 +0100

    Jak działa return keyword

--- 21. functions/main.js
+++ 21. functions/main.js
@@ -10,11 +10,21 @@ function happyBirthday(){
 }
 */
 // funkcja wymagająca pewnych argumentów (parametry funkcji)
-function happyBirthday(username, age){
+/*
+function happyBirthday(username, age){ //UWAGA!!! Kolejność ma znaczenie
     console.log("Happy birthday to you");
     console.log("Happy birthday to you");
     console.log(`Happy birthday dear ${username}`);
     console.log("Happy birthday to you");
     console.log(`You are ${age} years old`);
 }
-happyBirthday("kamerais", 19); // argumenty funkcji
+happyBirthday("kamerais", 19); // argumenty funkcj
+*/
+// Jak działa return keyword?
+function add(x,y){ // tworzę funkcję, która dodaje do siebie dwie wartości
+    let result = x + y; // tworzę zmienną, która przechowuje sumę dwóch wartości
+    return result; // zwracam wartość sumy
+    
+}
+let answer = add(2,3); // tworzę zmienną, która będzie przechowywać to co funkcja zwraca
+console.log(answer) // output: 5
