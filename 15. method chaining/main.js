95e46b36a74a0ea6b38a7baaa0c505a02b06d7d4
kamerais <kubickimarcin102@gmail.com>
2024-01-04 15:03:49 +0100

    Przykład programu bez pomocy method chaining-u

--- 15. method chaining/main.js
+++ 15. method chaining/main.js
@@ -3,7 +3,20 @@
 
 // ------- Bez Method Chaining-u -------
 
+let username = window.prompt("Enter your username");
 
+// Chcę, usunąć wszystkie spacje od lewej i od prawej strony
+// Pierwszą literę zrobić dużą, a reszta ma być z małych liter
+// Na końcu wyświetlić output
+username = username.trim();
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
 
+let extraChars = username.slice(1);
+extraChars = extraChars.toLowerCase();
+
+username = letter + extraChars;
+// input:     kaMErAiS     
+console.log(username); //output: Kamerais
 
 // ------- Z Method Chaining-iem -------
