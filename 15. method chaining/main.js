38897250e95b7891acacd913fb3f836822c875ce
kamerais <kubickimarcin102@gmail.com>
2024-01-04 15:07:13 +0100

    Przykład z method chaining-iem

--- 15. method chaining/main.js
+++ 15. method chaining/main.js
@@ -1,13 +1,14 @@
 // Method Chaining = wywoływanie metod jedna po drugiej
 //                   w jednej ciągłej linijce kodu
-
+let username = window.prompt("Enter your username");
 // ------- Bez Method Chaining-u -------
 
-let username = window.prompt("Enter your username");
+
 
 // Chcę, usunąć wszystkie spacje od lewej i od prawej strony
 // Pierwszą literę zrobić dużą, a reszta ma być z małych liter
 // Na końcu wyświetlić output
+/*
 username = username.trim();
 let letter = username.charAt(0);
 letter = letter.toUpperCase();
@@ -18,5 +19,10 @@ extraChars = extraChars.toLowerCase();
 username = letter + extraChars;
 // input:     kaMErAiS     
 console.log(username); //output: Kamerais
+*/
 
 // ------- Z Method Chaining-iem -------
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+// input:        kAMerAiS          
+console.log(username); //output Kamerais
