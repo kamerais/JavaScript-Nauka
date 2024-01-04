6727aafca5f4b90cfcc51435f59fd4e4827781f4
kamerais <kubickimarcin102@gmail.com>
2024-01-04 16:01:14 +0100

    Przykład zwykłego oraz ścisłego operatora porównania

--- 17. strict equality/main.js
+++ 17. strict equality/main.js
@@ -3,3 +3,21 @@
 // === strict equality operator (operator ścisłej równości) (sprawdza czy wartości i typ danych są takie same)
 // != inequality operator (operator nierówności)
 // !== strict inequality operator (operator ścisłej nierówności)
+
+const PI = 3.14;
+
+// przykład z ==
+if(PI == "3.14"){ // Wartości PI jest równa wartości w tekście (nawet jeżeli są to różne typy danych)
+    // program zatrzymuje się tutaj
+    console.log("That is PI");
+}
+else{
+    console.log("That is NOT PI");
+}
+// przykład z ===
+if(PI === "3.14"){ // Wartość PI mimo, że jest równa wartości w tekście to PI nie jest tekstem tylko liczbą
+    console.log("That is PI");
+}
+else{ // program zatrzymuje się tutaj
+    console.log("That is NOT PI");
+}
