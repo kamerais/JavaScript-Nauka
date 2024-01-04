1f5d9fc7d137d1d5d7b20cfa5cacea85fdcc32bf
kamerais <kubickimarcin102@gmail.com>
2024-01-04 16:14:34 +0100

    Przykład pętli nieskończonej

--- 18. while loops/main.js
+++ 18. while loops/main.js
@@ -1,1 +1,12 @@
 // while loop (pętla) = powtarza kod tyle raz ile razy DOPÓKI spełnia warunki
+
+let username ="";
+
+//Przykład pętli, która nigdy się nie skończy
+/*
+while (username ===""){ // warunek tutaj jest stale prawdziwy, dlatego wchodząc do pętli nigdy z niej nie wyjdziemy
+                        // no chyba, że pamięci zabraknie i program sam się zatrzyma
+    console.log("Enter your username");
+}
+console.log(`Hello ${username}`)
+*/
