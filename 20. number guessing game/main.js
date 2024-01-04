f1f83a3d9540d47952104ec5f1dce08968520ebe
kamerais <kubickimarcin102@gmail.com>
2024-01-04 18:12:35 +0100

    Logika gry do zgadywania liczby od podanego zakresu

--- 20. number guessing game/main.js
+++ 20. number guessing game/main.js
@@ -7,3 +7,29 @@ const answer = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
 let attempts = 0; // ilość podejść
 let guess; // zmienna, która będzie przyjmować user input
 let running = true;
+
+console.log(answer)
+while(running){
+    guess = window.prompt(`Welcome to the number guessing game. Enter a number from ${minValue} to ${maxValue}`);
+    guess = Number(guess);
+    
+    if(isNaN(guess)){ // NaN = Not a Number
+        window.alert("Please enter a valid number");
+    }
+    else if (guess < minValue || guess > maxValue){
+        window.alert("Please enter a valid number");
+    }
+    else{
+        attempts++;
+        if (guess < answer){
+            window.alert(`${guess} is bigger than looked number`);
+        }
+        else if (guess > answer){
+            window.alert(`${guess} is smaller than looked number`);
+        }
+        else{
+            window.alert(`Congratz. You guessed the number ${answer} correctly. It took you ${attempts}`);
+            running = false;
+        }
+    }
+}
