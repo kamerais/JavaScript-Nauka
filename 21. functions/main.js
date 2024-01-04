838b84f6b759bda097d97e2c77cc35ca9c623918
kamerais <kubickimarcin102@gmail.com>
2024-01-04 19:00:39 +0100

    Przykład jak napisac funkcję

--- 21. functions/main.js
+++ 21. functions/main.js
@@ -1,3 +1,11 @@
 // function (funkcja) = Sekcja reużywalnego kodu
 //                      Zadeklarowany raz kod, może zostać użyty kiedy chcesz
 //                      Wywołaj funkcję, abu uruchomić ten kod
+
+function happyBirthday(){
+    console.log("Happy birthday to you");
+    console.log("Happy birthday to you");
+    console.log("Happy birthday dear you");
+    console.log("Happy birthday to you");
+}
+happyBirthday()
