c2732f8c6911c236cce0e6479586d8fe1db2312a
kamerais <kubickimarcin102@gmail.com>
2024-01-05 01:02:56 +0100

    Logika programu do konwersji temperatury

--- 23. temperature conversion program/main.js
+++ 23. temperature conversion program/main.js
@@ -8,10 +8,15 @@ let temp;
 
 function convert(){
     if(toFahrenheit.checked){
-
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "℉";
+        // metoda .toFixed(liczba) pozwala na ustawienie z jaką dokładnością po przecinku ma się wyświetlać liczba
     }
     else if(toCelsius.checked){
-
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "℉";
     }
     else{
         result.textContent = "Select a unit";
