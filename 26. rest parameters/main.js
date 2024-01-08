5f20a9cabe493acf7918deea63bf7c3c2f11ed13
kamerais <kubickimarcin102@gmail.com>
2024-01-08 16:43:08 +0100

    Definicja rest parameters

---
+++ 26. rest parameters/main.js
@@ -0,0 +1,6 @@
+// rest parameters = (...rest) przyjmuje jakąś liczbę zmiennych jako agrumenty, z którymi pracuje
+//                             poprzez pakowanie ich do tablicy
+//
+//                             
+//                             spread = rozbija tablicę na pojedyńcze elementy
+//                             rest = łączy pojedyńcze elementy do tablicy
