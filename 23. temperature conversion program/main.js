f999e7dc54abb7a5605474fe499b69a4cdc4a8bd
kamerais <kubickimarcin102@gmail.com>
2024-01-08 12:54:25 +0100

    Alternatywna (ponoć lepsza praktycznie) wersja programu przez użytkownika z discorda

--- 23. temperature conversion program/main.js
+++ 23. temperature conversion program/main.js
@@ -22,3 +22,36 @@ function convert(){
         result.textContent = "Select a unit";
     }
 }
+/*
+Alternatywne napisanie tego programu (Zaproponował: pan1c z discorda)
+const textBoxValue = document.getElementById("textbox").value; z góry wiem, że będę pobierać wartość tego co wprowadzi użytkownik,
+                                                        dlatego daję .value, aby pobrać tę wartość
+const toFahrenheit = document.getElementById("toFahrenheit").checked; -,,- sprawdzać czy wybrana konwersja przez użytkownika
+                                                                    jest zaznaczona
+const toCelsius = document.getElementById("toCelsius").checked; -,,-
+const result = document.getElementById("result");
+
+function conver(){
+    if(!toFahrenheit && !toCelsius){
+        result.textContent = "Select a unit";
+        return;
+    }
+    const valueToNumber = Number(textBox);
+    else{
+        result.textContent = toFahrenheit
+        ? "$({textBoxValue * 9} / 5 + 32).toFixed(1)F"
+        : "$({textBoxValue - 32} * (5 / 9) ).toFixed(1)C";
+    }
+}
+W bardziej skróconej formie:
+function convert() {
+    const valueToNumber = Number(textBoxValue);
+    result.textContent =
+    !toFahrenheit && !toCelsius // najpierw pytam czy użytkownik NIE wybrał konwersji?
+    ? "Select a unit" // jeżeli NIE wybrał to result.textContent = "Select a unit";
+    : toFahrenheit jeżeli wybrał to pytam czy chce zamienić na Fahrenheity?
+    ? `${((valueToNumber * 9) / 5 + 32).toFixed(1)}F` // jeżeli tak to używam formuły z Celsiuszy na Fahrenheity
+    : `${((valueToNumber - 32) * (5 / 9)).toFixed(1)}C`; // w przeciwnym razie vice versa
+}
+
+*/
