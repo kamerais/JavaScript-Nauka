3d33d0605b32ba6881ac98dfaec0b47a42b3d881
kamerais <kubickimarcin102@gmail.com>
2024-01-08 16:48:00 +0100

    Przykład użycia rest parameters

--- 26. rest parameters/main.js
+++ 26. rest parameters/main.js
@@ -4,3 +4,14 @@
 //                             
 //                             spread = rozbija tablicę na pojedyńcze elementy
 //                             rest = łączy pojedyńcze elementy do tablicy
+
+function openFridge(...foods){
+    console.log(foods);
+}
+
+const food1 = "pizza";
+const food2 = "hamburger";
+const food3 = "hotdog";
+const food4 = "sushi";
+
+openFridge(food1, food2, food3, food4); //output: ["pizza", "hamburger", "hotdog", "sushi"]
