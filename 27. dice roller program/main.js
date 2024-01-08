9860449ea4daab221ac7edf4fe2a5983886e641d
kamerais <kubickimarcin102@gmail.com>
2024-01-08 19:12:32 +0100

    Stworzenie logiki programu

--- 27. dice roller program/main.js
+++ 27. dice roller program/main.js
@@ -1,5 +1,18 @@
 // DICE ROLLER PROGRAM
 
 function rollDice(){
-    const numOfDice = document.getElementById("numOfDice");
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="pics/Dice-${value}.png" alt="Dice ${value}">`);
+    }
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
+    console.log(images);
 }
