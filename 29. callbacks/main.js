1b3660c4df55af3591339475f0106d02d696ba87
kamerais <kubickimarcin102@gmail.com>
2024-01-08 21:13:03 +0100

    Przykłady callback-ów

--- 29. callbacks/main.js
+++ 29. callbacks/main.js
@@ -6,3 +6,46 @@
 //            3. Interaktowanie z bazami danych
 
 //            "Hey, when you're done, call this next" - to mówi JS do callbacka
+hello(wait, leave, goodbye);
+
+
+function hello(callWait, callLeave, callBye){
+    console.log("Hello"); // output: Hello
+    callWait(); // output: Wait
+    callLeave(); // output: Leave!
+    callBye(); // output: Goodbye
+}
+/*
+Jeżeli ja zmienię kolejność wykonywanych funkcji, to i kolejność wyświetlanego tekstu się zmieni
+np.
+function hello(callWait, callLeave, callBye){
+    callWait(); // output: Wait
+    console.log("Hello"); // output: Hello
+    callBye(); // output: Goodbye
+    callLeave(); // output: Leave!
+    
+}
+*/
+
+function wait(){
+    console.log("Wait");
+}
+
+function leave(){
+    console.log("Leave!");
+}
+
+
+function goodbye(){
+    console.log("Goodbye");
+}
+
+sum(displayConsole, 1, 2); // output: 3
+
+function sum(callback, x, y){
+    let result = x + y;
+    callback(result);
+}
+function displayConsole(result){
+    console.log(result);
+}
