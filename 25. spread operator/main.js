4a93105f1f4f89a8b65132a105540dd61ff8b966
kamerais <kubickimarcin102@gmail.com>
2024-01-08 16:29:53 +0100

    Inne przykłady korzystania ze spread operator-a

--- 25. spread operator/main.js
+++ 25. spread operator/main.js
@@ -21,10 +21,31 @@ let username = "kamerais"; // ustalam mój username
 let letters = [...username]; // za pomocą ... wypakowuje litery z mojego tekstu pojedyńczo i wkładam je do tej tablicy osobno
 
 console.log(letters); // output: ["k", "a", "m", "e", "r", "a", "i", "s"]
-console.log(typeof letters); //output object
+console.log(typeof letters); //output: object
 
 // mogę sobie połączyć te litery jakimś znakiem np. "-"
 
 letters = letters.join("-");
 console.log(letters); // output: k-a-m-e-r-a-i-s
-console.log(typeof letters); //output string
+console.log(typeof letters); //output: string
+
+// można ze stworzonej tablicy przenieść POJEDYŃCZO jej elementy do innej tablicy
+
+let fruits = ["apple", "orange", "banana"];
+
+console.log(`To jest tablica fruits. Jej elementy ${fruits}`); // output: To jest tablica fruits. Jej elementy ["apple", "orange", "banana"]
+
+// teraz chcę te same elementy z tablicy fruits do nowej tablicy
+
+let newFruits = [...fruits];
+
+console.log(`To jest tablica newFruits. Jej elementy ${newFruits}`); // output: To jest tablica newFruits. Jej elementy ["apple", "orange", "banana"]
+
+// mogę tak samo zrobić z dodatkowymi tablicami, aby upakować je w jedną tablicę
+
+let vegetables = ["carrots", "celery", "potatoes"];
+
+let food = [...fruits, ...vegetables];
+
+console.log(`To jest tablica newFruits. Jej elementy ${food}`); // output: To jest tablica newFruits. Jej elementy
+//                                                              ["apple", "orange", "banana", "carrots", "celery", "potatoes"]
