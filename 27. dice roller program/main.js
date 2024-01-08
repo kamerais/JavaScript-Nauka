41858e96aadd75064eb6bdb33cd3042df4997698
kamerais <kubickimarcin102@gmail.com>
2024-01-08 19:17:37 +0100

    Opisanie co się dzieje w programie

--- 27. dice roller program/main.js
+++ 27. dice roller program/main.js
@@ -1,18 +1,19 @@
 // DICE ROLLER PROGRAM
 
 function rollDice(){
-    const numOfDice = document.getElementById("numOfDice").value;
+    const numOfDice = document.getElementById("numOfDice").value; // pobieram wartość z inputa
     const diceResult = document.getElementById("diceResult");
     const diceImages = document.getElementById("diceImages");
-    const values = [];
-    const images = [];
+    const values = []; // tworzę tablicę, która przechowuje wartości kostek
+    const images = []; // tworzę tablicę, która przechowuje zdjęcia kostek
 
     for(let i = 0; i < numOfDice; i++){
-        const value = Math.floor(Math.random() * 6) + 1;
-        values.push(value);
-        images.push(`<img src="pics/Dice-${value}.png" alt="Dice ${value}">`);
+        const value = Math.floor(Math.random() * 6) + 1; // tworzę zmienną losową od 1-6
+        values.push(value); // na koniec listy dodaje wylosowaną wartość
+        images.push(`<img src="pics/Dice-${value}.png" alt="Dice ${value}">`); // na koniec listy dodaję img taga z wysolowaną wartością kostki
+        //                                                                        aby wyświetlić je na stronie
     }
-    diceResult.textContent = `dice: ${values.join(', ')}`;
-    diceImages.innerHTML = images.join('');
+    diceResult.textContent = `dice: ${values.join(', ')}`; // oddzielam wszystkie liczby przecinskiem
+    diceImages.innerHTML = images.join(''); // oddzielam obrazki pustym stringiem
     console.log(images);
 }
