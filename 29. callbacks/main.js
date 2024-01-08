8a40091036c6aba41406e93038a4f630a5e4deee
kamerais <kubickimarcin102@gmail.com>
2024-01-08 21:00:54 +0100

    Definicja callback-ów

---
+++ 29. callbacks/main.js
@@ -0,0 +1,8 @@
+// callback - funkcja, która jest wprowadzana jako argument dla innej funkcji
+
+//            używana jest do obchodzenia się z asynchronicznymi operacjami:
+//            1. Czytanie pliku
+//            2. Network requests
+//            3. Interaktowanie z bazami danych
+
+//            "Hey, when you're done, call this next" - to mówi JS do callbacka
