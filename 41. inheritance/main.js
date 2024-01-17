3c920e225bbab5b4b160d9cea59a9ed93391eb54
kamerais <kubickimarcin102@gmail.com>
2024-01-17 16:15:24 +0100

    Definicja dziedziczenia

---
+++ 41. inheritance/main.js
@@ -0,0 +1,3 @@
+// inheritance (dziedziczenie) = pozwala nowej klasie dziedziczyć własności oraz metody
+//                               z innej istniejącej klasy (rodzic -> dziecko)
+//                               pomaga przy reużywalności kodu
