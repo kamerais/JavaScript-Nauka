2ae3cbbc6dc4f84b4f8fed23d55a0b856371367c
kamerais <kubickimarcin102@gmail.com>
2024-01-17 12:38:09 +0100

    Przykład arrow expression

--- 35. arrow function/main.js
+++ 35. arrow function/main.js
@@ -1,3 +1,11 @@
 // arrow functions = krótszy sposób na napisanie fuction expressions
 //                   dobre dla prostych funkcji, które zostaną użyte tylko raz
-//                   (parametry) =>{ jakiś kod }
+//                   (parametry) => { jakiś kod }
+
+// Przykład arrow function
+
+const hello = (name,age) => {console.log(`Hello ${name}`)
+                            console.log(`You are ${age} years old`)
+                            };
+
+hello("kamerais",19)
