24a46e2062397c88feb88b77e125618bb3eba48e
kamerais <kubickimarcin102@gmail.com>
2024-01-17 12:55:22 +0100

    Dodaje przykład Patryka jako object

--- 36. JavaScript objects/main.js
+++ 36. JavaScript objects/main.js
@@ -2,13 +2,26 @@
 //          może reprezentować objekty ze świata prawdziwego (ludzie, produkty, miejsca)
 //          object = {key: value,function()}
 
-const person = {
+const person1 = {
     firstName: "Spongebob",
     lastName: "Squarepants",
     age: 30,
     isEmployed: true,
 }
-console.log(person.firstName) // output: Spongebob
-console.log(person.lastName) // output: Squarepants
-console.log(person.age) // output: 30
-console.log(person.isEmployed) // output: true
+
+console.log(person1.firstName) // output: Spongebob
+console.log(person1.lastName) // output: Squarepants
+console.log(person1.age) // output: 30
+console.log(person1.isEmployed) // output: true
+
+const person2 ={
+    firstName: "Patrick",
+    lastName: "Star",
+    age: 42,
+    isEmployed: false,
+}
+
+console.log(person2.firstName) // output: Patrick
+console.log(person2.lastName) // output: Star
+console.log(person2.age) // output: 42
+console.log(person2.isEmployed) // output: false
