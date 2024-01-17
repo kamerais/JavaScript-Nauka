f19a9ca83c9a06ee0bceed06cbf3b73405c365da
kamerais <kubickimarcin102@gmail.com>
2024-01-17 18:14:29 +0100

    Przykład 1

--- 44. Destructuring/main.js
+++ 44. Destructuring/main.js
@@ -4,3 +4,20 @@
 //                 {} = aby wykonać object destructuring
 //                 5 przykładów
 
+// -------- Przykład  1 ----------
+// Zamień wartości między dwiema zmiennymi
+
+let a = 1;
+let b = 2;
+// przed destrukturyzacją
+
+console.log(a); // output: 1
+console.log(b); // output: 2
+
+// dokonuję destrukturyzacji
+[a, b] = [b, a]; // po lewej stronie korzystamy z destructuring a po prawej tworzymy tablicę
+
+// po destrukturyzacji
+console.log(a); // output: 2
+console.log(b); // output: 1
+
