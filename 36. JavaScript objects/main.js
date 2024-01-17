f96ac9ba1e85b0cc928e3106e90ff9ac3af771d8
kamerais <kubickimarcin102@gmail.com>
2024-01-17 12:50:12 +0100

    Definicja objektu

---
+++ 36. JavaScript objects/main.js
@@ -0,0 +1,3 @@
+// object = To kolekcja powiązanych ze sobą własności i/lub metod
+//          może reprezentować objekty ze świata prawdziwego (ludzie, produkty, miejsca)
+//          object = {key: value,function()}
