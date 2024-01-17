36dccc9668a63168b2de19ad003a1d2c648a7059
kamerais <kubickimarcin102@gmail.com>
2024-01-17 13:12:59 +0100

    Przykład użycia THIS keyword

--- 37. what is THIS/main.js
+++ 37. what is THIS/main.js
@@ -2,3 +2,10 @@
 //        (objekt jest zależny od natychmiastowego kontekstu)
 //        person.name = this.name
 
+const person1 = {
+    name: "SpongeBob",
+    favFood: "hamburgers",
+    sayHello: function(){console.log(`Hi! I am ${this.name}`)} // bez this. to output: Hi! I am 
+}
+
+person1.sayHello(); // output: Hi! I am SpongeBob
