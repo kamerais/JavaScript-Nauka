3a5295666d3b222f93cc156103b546f7766cc399
kamerais <kubickimarcin102@gmail.com>
2024-01-17 12:15:51 +0100

    Definicja i różnice function expressions i function declaration

---
+++ 34. function expressions/main.js
@@ -0,0 +1,5 @@
+// function declaration = definiuje reużywalny blok kodu
+//                        który wykonuje konkretne polecenia
+
+// function expression = sposób na zdefiniowanie funkcji
+//                       jako wartości lub zmiennych
