51aabb3fe4de3cb002ef6c15e41eb466ceeb0cb7
kamerais <kubickimarcin102@gmail.com>
2024-01-17 14:42:41 +0100

    Definicja klasy

---
+++ 39. classes/main.js
@@ -0,0 +1,3 @@
+// class = (ES6 feature) wprowadza bardziej structured i czystszy sposób
+//         do pracy z obiektami w porównaniu do tradycyjnych constructor functions
+//         np. static keyword, inkapsulację, dziedziczenie
