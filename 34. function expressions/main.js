e8cc6007870f1d4d4f35a35d1491d8e4c69e6105
kamerais <kubickimarcin102@gmail.com>
2024-01-17 12:19:36 +0100

    Przykład function expression i function declaration

--- 34. function expressions/main.js
+++ 34. function expressions/main.js
@@ -3,3 +3,16 @@
 
 // function expression = sposób na zdefiniowanie funkcji
 //                       jako wartości lub zmiennych
+
+// Przykład function declaration
+function hello1(){
+    console.log("Hello");
+}
+hello1();
+// Przykład fuction expression
+const hello2 = function(){
+    console.log("Hello")
+}
+hello2()
+// Jeżeli jest funckja o tej samej nazwie jest już zadeklarowana to wyrzuci błąd
+// dlatego jest hello1()i hello2()
