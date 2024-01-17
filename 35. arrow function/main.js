5b9b0d426b41afa118ac5f89e3838ac08e68dfa2
kamerais <kubickimarcin102@gmail.com>
2024-01-17 12:34:20 +0100

    Uzupełnienie HTMLa i definicja arrow functions

---
+++ 35. arrow function/main.js
@@ -0,0 +1,3 @@
+// arrow functions = krótszy sposób na napisanie fuction expressions
+//                   dobre dla prostych funkcji, które zostaną użyte tylko raz
+//                   (parametry) =>{ jakiś kod }
