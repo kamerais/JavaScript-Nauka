b6745855b6f41148c393aa4c9523cb11ed7f9721
kamerais <kubickimarcin102@gmail.com>
2024-01-17 12:59:01 +0100

    Przykład dodania funkcji dla obu objektów

--- 36. JavaScript objects/main.js
+++ 36. JavaScript objects/main.js
@@ -7,21 +7,25 @@ const person1 = {
     lastName: "Squarepants",
     age: 30,
     isEmployed: true,
+    sayHello: function(){console.log("Hi I'm Spongebob")},
 }
 
-console.log(person1.firstName) // output: Spongebob
-console.log(person1.lastName) // output: Squarepants
-console.log(person1.age) // output: 30
-console.log(person1.isEmployed) // output: true
+console.log(person1.firstName); // output: Spongebob
+console.log(person1.lastName); // output: Squarepants
+console.log(person1.age); // output: 30
+console.log(person1.isEmployed); // output: true
+person1.sayHello(); // output: Hi I'm Spongebob
 
 const person2 ={
     firstName: "Patrick",
     lastName: "Star",
     age: 42,
     isEmployed: false,
+    sayHello: () => {console.log("Hi I'm Patrick...")},
 }
 
-console.log(person2.firstName) // output: Patrick
-console.log(person2.lastName) // output: Star
-console.log(person2.age) // output: 42
-console.log(person2.isEmployed) // output: false
+console.log(person2.firstName); // output: Patrick
+console.log(person2.lastName); // output: Star
+console.log(person2.age); // output: 42
+console.log(person2.isEmployed); // output: false
+person2.sayHello();
