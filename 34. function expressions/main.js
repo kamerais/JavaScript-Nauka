ff9c061b736b917fd5370951e14370a702e6dfc0
kamerais <kubickimarcin102@gmail.com>
2024-01-17 12:23:52 +0100

    Przykład function expressions jako argument innej funkcji

--- 34. function expressions/main.js
+++ 34. function expressions/main.js
@@ -16,3 +16,11 @@ const hello2 = function(){
 hello2()
 // Jeżeli jest funckja o tej samej nazwie jest już zadeklarowana to wyrzuci błąd
 // dlatego jest hello1()i hello2()
+
+
+// function expression można wprowadzać jako argument w innych funkcjach
+// setTimeout(callback, ilość sekund w ms) = opóźnia uruchomienie funkcji o jakiś czas w ms
+
+setTimeout(function(){
+    console.log("Hello po 3 sekundach");
+},3000); // 3000 ms = 3s
