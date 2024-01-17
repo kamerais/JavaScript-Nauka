f41b9d437fe34b4a16f1a62ac0889cc09e00e59a
kamerais <kubickimarcin102@gmail.com>
2024-01-17 18:10:29 +0100

    Definicja destrukturyzacji

---
+++ 44. Destructuring/main.js
@@ -0,0 +1,6 @@
+// destructuring = wydobywa wartości z tablic i objektów,
+//                 wtedy przypisuje je do zmiennych w dogodny sposób
+//                 [] = aby wykonać array destructuring
+//                 {} = aby wykonać object destructuring
+//                 5 przykładów
+
