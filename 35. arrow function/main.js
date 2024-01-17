587482135b4207f3d2703274f5a41e8ab614d420
kamerais <kubickimarcin102@gmail.com>
2024-01-17 12:45:49 +0100

    Przykłady arrow functions w map,reduce i filter na listach

--- 35. arrow function/main.js
+++ 35. arrow function/main.js
@@ -8,4 +8,30 @@ const hello = (name,age) => {console.log(`Hello ${name}`)
                             console.log(`You are ${age} years old`)
                             };
 
-hello("kamerais",19)
+hello("kamerais",19);
+
+// Przykład arrow function jako argument
+
+setTimeout(() => {
+    console.log("Hello");
+},3000);
+/*
+    Jeżeli jest to kod jedno linijkowy można napisać zwykłe
+    setTimeout(() => console.log("Hello");, 3000)
+*/
+
+// Przykład arrow function przy map,reduce i filter
+
+const numbers = [1,2,3,4,5,6];
+
+const squares = numbers.map((element) => Math.pow(element, 2));
+
+console.log(squares); // output: [1, 4, 9, 16, 25, 36]
+
+const evenNumbers = numbers.filter((element) => element % 2 === 0);
+
+console.log(evenNumbers); // output [2, 4, 6]
+
+const total = numbers.reduce((accumulator, element) => accumulator + element);
+
+console.log(total); // output: 21
