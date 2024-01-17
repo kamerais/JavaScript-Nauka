de57b5cde7768b6cdee85df85eaa938941548690
kamerais <kubickimarcin102@gmail.com>
2024-01-17 12:53:08 +0100

    Przykład Spongebob-a jako obiekt

--- 36. JavaScript objects/main.js
+++ 36. JavaScript objects/main.js
@@ -1,3 +1,14 @@
 // object = To kolekcja powiązanych ze sobą własności i/lub metod
 //          może reprezentować objekty ze świata prawdziwego (ludzie, produkty, miejsca)
 //          object = {key: value,function()}
+
+const person = {
+    firstName: "Spongebob",
+    lastName: "Squarepants",
+    age: 30,
+    isEmployed: true,
+}
+console.log(person.firstName) // output: Spongebob
+console.log(person.lastName) // output: Squarepants
+console.log(person.age) // output: 30
+console.log(person.isEmployed) // output: true
