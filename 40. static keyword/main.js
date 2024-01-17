1119af3027240dbe4c123ba14c5ef2112f55e27a
kamerais <kubickimarcin102@gmail.com>
2024-01-17 15:51:08 +0100

    Definicja static keyword

---
+++ 40. static keyword/main.js
@@ -0,0 +1,3 @@
+// static = słowoklucz (keyword), który definiuje własności i metody, które należą
+//          do klasy samej w sobie niż dla obiektów stworzonych przy użyciu tej klasy
+//          (klasa posiada to co statyczne, obiekty już nie)
