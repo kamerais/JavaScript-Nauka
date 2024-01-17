9489de0e05feb9a9438f15fac3f9850676ca3836
kamerais <kubickimarcin102@gmail.com>
2024-01-17 18:04:56 +0100

    Przykłady użycia getters i setters

--- 43. getters and setters/main.js
+++ 43. getters and setters/main.js
@@ -10,7 +10,8 @@ class Rectangle{
     }
     set width(newWidth){
         if (newWidth > 0){
-            return this._newWidth = newWidth; // _ przed newWidth powoduje, że ta zmienna jest sekretna
+            return this._width = newWidth; // _ przed newWidth powoduje, że ta zmienna jest prywatna
+                                           // i mówi innym deweloperom, że nie powinno się jej tykać
         }
         else{
             console.error("Width must be a positive number");
@@ -18,11 +19,21 @@ class Rectangle{
     }
     set height(newHeight){
         if (newHeight > 0){
-            return this._newHeight = newHeight; // _ przed newHeight powoduje, że ta zmienna jest sekretna
+            return this._height = newHeight; // _ przed newHeight powoduje, że ta zmienna jest prywatna
+                                            // i mówi innym deweloperom, że nie powinno się jej tykać
         }
         else{
             console.error("Height must be a positive number");
         }
+    }
+    get width(){
+        return `${this._width.toFixed(1)}cm`;
+    }
+    get height(){
+        return `${this._height.toFixed(1)}cm`;
+    }
+    get area(){
+        return `${(this._width * this._height).toFixed(1)}cm^2`;
     }
 }
 /*
@@ -33,3 +44,7 @@ Wyskakują 2 errory:
     - Width must be a positive number
     - Height must be a positive number
 */
+const rectangle = new Rectangle(4,3);
+console.log(rectangle.width); // output: 4.0cm
+console.log(rectangle.height); // output: 3.0cm
+console.log(rectangle.area); // output: 12.0cm^2
