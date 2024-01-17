1f85d0fed94a9b2cfff239a29b48d6eb63380332
kamerais <kubickimarcin102@gmail.com>
2024-01-17 16:29:51 +0100

    Definicja super keyword

---
+++ 42. super keyword/main.js
@@ -0,0 +1,4 @@
+// super = keyword używane w klasach do wywołania konstruktora lub
+//         dostać dostęp do właściwości i metod z klasy rodzicielskiej (superclass)
+//         this = ten objekt
+//         super = rodzic
