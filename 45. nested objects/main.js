4554b60886590799b27d2873045c955fda891bc2
kamerais <kubickimarcin102@gmail.com>
2024-01-17 19:41:19 +0100

    Definicja nested objects

---
+++ 45. nested objects/main.js
@@ -0,0 +1,6 @@
+// nested objects = Obiekty wewnątrz innych obiektów
+//                  Pozwalają ci na reprezentowanie bardziej skomplikowanych struktur danych
+//                  Objekt dziecko is załączony przez rodzicielski Objekt
+
+//                  Person{Address{}, ContactInfo{}}
+//                  ShoppingCart{Keyboard{}, Mouse{}, Monitor{}}
