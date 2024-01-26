388c23dad553d1f94d07aad17749d611bc1d7a86
kamerais <kubickimarcin102@gmail.com>
2024-01-26 09:30:40 +0100

    Definicja metody sortowania

---
+++ 47. sorting/main.js
@@ -0,0 +1,3 @@
+// sort() = metoda używana do sortowania elementów tablicy w odpowiednim porządku
+//          Sortuje elementy jako stringi w porządku leksykograficznym, nie alfabetycznym
+//          leksykograficzny = (alfabet + numerki + symbole) jako string
